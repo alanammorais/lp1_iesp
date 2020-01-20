b686d0f1ca36ecdc82015440106b15cc6562f051
Alana MM <alanamm.prof@gmail.com>
2020-01-20 16:35:21 -0300

    new semester

--- exemplos/Aula2002.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main(){
-    printf("Ola Mundo");
-
-}
--- exemplos/primeiro.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int periodo = 2;
-    printf ("Turma 2019.%i", periodo);
-    printf("\n%s\n%s","teste1", "outra string");
-    return 0;
-}
